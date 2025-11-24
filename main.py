529edaf31508ab20b0d7e227ca5fb828b9dce67f
im25a-charefe <charefe@bzz.ch>
2025-11-24 13:21:22 +0100

    ich habe ein bisschen gemacht

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 #Spielinstrucktionen
-
+import random
 
 #alle Inputs und Variabeln(anzahl Punkte, Spieler...)
 pointbarrier = int(input("What's the point barrier? "))
 dicecount = int(input("How many dice do you have? "))
+counter = 0
+roll = 0
+players = []
+rolls = []
+def playerselection():
+    while 'done' not in players:
+        player_name = input("What's the player name? ")
+        players.append(player_name)
+    players.pop()
 
-#loop wo alle players hinzugefügt werden
+playerselection()
+print(players)
 
-#Mainfunktion
-#   jeder spieler durchläuft den folgenden loop
+for player in players:
+    decision = input(f'do you want to roll {player}? (y or n) ')
+    if decision == 'y':
+        while dicecount > counter:
+            roll = random.randint(1, 6)
+            rolls.append(roll)
+            counter += 1
+        counter = 0
+        total = sum(rolls)
+        print(f'your total is {total} these are your rolls:{rolls}')
+
 """
     print: Spieler ... ist am zug
     print: drücken sie enter um zu würfeln oder space um aufzuhören.
