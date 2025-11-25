bf3728af1603ec884211b8f35b83a48e43f2b1dd
im25a-charefe <charefe@bzz.ch>
2025-11-25 20:02:54 +0100

    ich habe änderungen gemacht

--- main.py
+++ main.py
@@ -7,34 +7,57 @@ dicecount = int(input("How many dice do you have? "))
 counter = 0
 roll = 0
 players = {}
+players_final_score = {}
 rolls = []
 def playerselection():
-    while 'done' not in players:
+    while 'done' not in players and 'Done' not in players:
         player_name = input("What's the player name? ")
         players[player_name] = 0
-    players.pop('done')
+    players.popitem()
 
 playerselection()
-print(players.keys())
+print(list(players.keys()))
+def gameloop():
+    global counter
+    global roll
+    global rolls
+    global players
+    global players_final_score
 
-for player in players:
-    decision = input(f'do you want to roll {player}? (y or n) ')
-    if decision == 'y':
-        while dicecount > counter:
-            roll = random.randint(1, 6)
-            rolls.append(roll)
-            counter += 1
-        counter = 0
-        total = sum(rolls)
-        print(f'your total is {total} these are your rolls:{rolls}')
-        rolls = []
+    for player in players:
+        if players[player] >= 0:
+            decision = input(f'do you want to roll {player}? (y or n) ')
+            if decision == 'y':
+                while dicecount > counter:
+                    roll = random.randint(1, 6)
+                    rolls.append(roll)
+                    counter += 1
 
-"""
-    print: Spieler ... ist am zug
-    print: drücken sie enter um zu würfeln oder space um aufzuhören.
-    nach jedem wurf wird der aktuelle score revealed
-    zusätzlich sieht man rechts die point barrier
-    Fals Zeit besteht kann ein feature ein gebaut werden dass zeigt wo du in der rangliste
-    wärst wenn du jetzt rausgehst
-"""
-#Auswertung und Rangliste(Scorebord)
+                counter = 0
+                total = sum(rolls)
+                players[player] += total
+
+                if players[player] >= pointbarrier:
+                    lost = ' (you lost)'
+                    players[player] = -1
+                    players_final_score[player] = players[player]
+
+                else:
+                    lost = ''
+
+                print(f'your total is {total} these are your rolls:{rolls}{lost}')
+                rolls = []
+
+            elif decision == 'n':
+                players_final_score[player] = players[player]
+
+    for player in players_final_score:
+        if player in players:
+            del players[player]
+while len(players) >= 1:
+    gameloop()
+
+print(players)
+sorted_players = dict(sorted(players.items(), key=lambda item: item[1], reverse=True))
+print(sorted_players.keys())
+print (players_final_score)
