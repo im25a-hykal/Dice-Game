81a83aefc25ef35932ae52bc60a67d765e9c9516
im25a-meyera2 <meyera2@bzz.ch>
2025-11-27 11:20:40 +0100

    I made the welcome sign and started with the bank def

--- main2.py
+++ main2.py
@@ -1,7 +1,20 @@
 #Spielinstrucktionen
 import random
 
+
+def welcome():
+    print("Welcome to GOAD!")
+    print('The rules are simple.')
+    print('You can play with multiple dice and set the points by yourself.')
+    print('You play against the Bank and the other players. If your points are higher or equal than the max you lose.')
+    print('You can decide if you want to throw the dices')
+    print('The player who is the closest to the set max wins')
+    print('The game GOAD, Greatest Of All Dice starts now')
+    print('Made by Riccardo, Elia, Lenny, Marvin and Arun ')
+    print('')
+
 #alle Inputs und Variabeln(anzahl Punkte, Spieler...)
+welcome()
 pointbarrier = int(input("What's the point barrier? "))
 dicecount = int(input("How many dice do you have? "))
 counter = 0
@@ -11,7 +24,7 @@ players_final_score = {}
 rolls = []
 def playerselection():
     while 'done' not in players and 'Done' not in players:
-        player_name = input("What's the player name? ")
+        player_name = input("What's the player name?  If you are done type 'done'")
         players[player_name] = 0
     players.popitem()
 
@@ -24,9 +37,18 @@ def gameloop():
     global players
     global players_final_score
 
+
+
+    #def bank():
+        #while dicecount > counter:
+            #roll = random.randint(1, 6)
+            #rolls.append(roll)
+            #counter += 1
+
+
     for player in players:
         if players[player] >= 0:
-            decision = input(f'do you want to roll {player}? (y or n) ')
+            decision = input(f'do you want to roll {player} (your current points {players[player]})? (y or n) ')
             if decision == 'y':
                 while dicecount > counter:
                     roll = random.randint(1, 6)
